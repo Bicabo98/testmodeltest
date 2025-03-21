ee064535f1b7204e67372a227c16274357496131
haike0513 <haike0513@gmail.com>
2025-03-21 09:54:54 +0800

    chroe: update endpoint

--- src/lib/haxios.ts
+++ src/lib/haxios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { getAuthToken } from './auth';
 
-export const BASE_URL = process.env.API_ENDPOINT || 'http://213.136.84.124:8080';
-// export const BASE_URL = process.env.API_ENDPOINT || 'https://nftkash.xyz';
+// export const BASE_URL = process.env.API_ENDPOINT || 'http://213.136.84.124:8080';
+export const BASE_URL = process.env.API_ENDPOINT || 'https://nftkash.xyz';
 
 export const haxios = axios.create({
   baseURL: BASE_URL,
